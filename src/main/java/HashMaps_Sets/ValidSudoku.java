c57cadf7a257e6f4b872d97675a8e2dd9854a831
Gustavo Gião <gustavogiao@ua.pt>
2025-03-26 23:31:54 +0000

    resolved valid sudoku

---
+++ src/main/java/HashMaps_Sets/ValidSudoku.java
@@ -0,0 +1,62 @@
+package HashMaps_Sets;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        boolean result = isValidSudoku(board);
+        System.out.println("O tabuleiro é válido? " + result);
+    }
+
+    public static boolean isValidSudoku(char[][] board){
+
+        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char value = board[r][c];
+
+                if(value == '.') continue;
+
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r].contains(value) || cols[c].contains(value) || boxes[boxIndex].contains(value)) {
+                    return false;
+                }
+
+                rows[r].add(value);
+                cols[c].add(value);
+                boxes[boxIndex].add(value);
+
+            }
+            
+        }
+
+        return true;
+
+    }
+
+}
